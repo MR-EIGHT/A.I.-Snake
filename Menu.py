345a2f45bf57b3eccec4b2d645fb63f05690a53d
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2022-04-09 00:50:19 +0430

    implemented a simple menu for the game.

---
+++ Menu.py
@@ -0,0 +1,42 @@
+import pygame_menu
+from pygame_menu.examples import create_example_window
+from typing import Tuple
+import UI
+
+surface = create_example_window('Snake Game', (600, 400))
+algorithm = 'A*'
+
+
+def set_algorithm(selected: Tuple) -> None:
+    global algorithm
+    algorithm = selected[0][0]
+
+
+def start_the_game() -> None:
+    """
+    Function that starts a game. This is raised by the menu button,
+    here menu can be disabled, etc.
+    """
+
+    # menu.disable()
+    menu.full_reset()
+    UI.main(algorithm)
+    menu.close()
+
+
+menu = pygame_menu.Menu(
+    height=300,
+    theme=pygame_menu.themes.THEME_ORANGE,  # you can set the menu's theme here.
+    title='Snake Game',
+    width=400
+)
+
+menu.add.selector('Algorithm: ', [('A*', 1), ('BFS', 2)], onchange=set_algorithm)
+menu.add.button('Play', start_the_game)
+menu.add.button('Quit', pygame_menu.events.EXIT)
+
+
+def show_menu():
+    global surface
+    surface = create_example_window('Snake Game', (600, 400))
+    menu.mainloop(surface)
